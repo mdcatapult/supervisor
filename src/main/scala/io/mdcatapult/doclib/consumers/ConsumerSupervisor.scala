62e67133dfb2cca0db7fb0f02fe739690f405256
Ian Dunlop <ian.dunlop@md.catapult.org.uk>
2019-10-17 16:38:12 +0100

    Include the codecs

--- src/main/scala/io/mdcatapult/doclib/consumers/ConsumerSupervisor.scala
+++ src/main/scala/io/mdcatapult/doclib/consumers/ConsumerSupervisor.scala
@@ -7,8 +7,10 @@ import com.typesafe.scalalogging.LazyLogging
 import io.mdcatapult.doclib.handlers.SupervisorHandler
 import io.mdcatapult.doclib.messages.SupervisorMsg
 import io.mdcatapult.doclib.models.DoclibDoc
+import io.mdcatapult.doclib.util.MongoCodecs
 import io.mdcatapult.klein.mongo.Mongo
 import io.mdcatapult.klein.queue.Queue
+import org.bson.codecs.configuration.CodecRegistry
 import org.mongodb.scala._
 
 import scala.concurrent.ExecutionContextExecutor
@@ -23,11 +25,12 @@ object ConsumerSupervisor extends App with LazyLogging {
   implicit val config: Config = ConfigFactory.load()
 
   /** Initialise Mongo **/
+  implicit val codecs: CodecRegistry = MongoCodecs.get
   implicit val mongo: Mongo = new Mongo()
   implicit val collection: MongoCollection[DoclibDoc] = mongo.database.getCollection(config.getString("mongo.collection"))
 
   /** initialise queues **/
-  val upstream: Queue[SupervisorMsg] = new Queue[SupervisorMsg](config.getString("upstream.queue"))
+  val upstream: Queue[SupervisorMsg] = new Queue[SupervisorMsg](config.getString("upstream.queue"), Option(config.getString("op-rabbit.topic-exchange-name")))
   val subscription: SubscriptionRef = upstream.subscribe(new SupervisorHandler(upstream).handle, config.getInt("upstream.concurrent"))
 
 }
