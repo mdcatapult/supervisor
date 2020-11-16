4651c6f59a4e25246f9fe6c8161232dd71a8b184
daniel.james <daniel.james@md.catapult.org.uk>
2020-11-16 13:09:52 +0000

    Bypasses doTask for analytical.supervisor

--- src/main/scala/io/mdcatapult/doclib/rules/sets/Analytical.scala
+++ src/main/scala/io/mdcatapult/doclib/rules/sets/Analytical.scala
@@ -20,7 +20,7 @@ object Analytical extends SupervisorRule[DoclibMsg] {
   override def unapply(doc: DoclibDoc)(implicit config: Config, registry: Registry[DoclibMsg]): Option[(String, Sendables)] =
     if (config.getBoolean("analytical.supervisor"))
       //doTask("supervisor.analytical", doc)
-      Some(("supervisor.analytical", List(Sendable(registry.get("analytical.supervisor")))))
+      Some(("supervisor.analytical", List(registry.get("analytical.supervisor")).asInstanceOf[Sendables]))
     else
       None
 }
